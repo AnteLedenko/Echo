86791409d943d88b6dc152207fb7ef48698cd5ee
Ante Ledenko <ledenk96@gmail.com>
2025-04-08 22:13:03 +0100

    update 5

--- frontend/src/utils/wsBase.js
+++ frontend/src/utils/wsBase.js
@@ -1,5 +1,4 @@
-const WS_BASE = import.meta.env.DEV
-  ? "ws://127.0.0.1:8000/ws/chat"
-  : `wss://${import.meta.env.VITE_BACKEND_HOST}/ws/chat`;
+const WS_BASE = `wss://${import.meta.env.VITE_BACKEND_URL.replace("/api", "").replace("https://", "")}/ws/chat`;
 
 export default WS_BASE;
+
