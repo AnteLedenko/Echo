bff3ad55115a668fa60b1603db7cc14181d75bfd
Ante Ledenko <ledenk96@gmail.com>
2025-04-09 19:09:37 +0100

    update 13

--- backend/echo/categories/migrations/0006_populate_cloudinary_categories.py
+++ backend/echo/categories/migrations/0006_populate_cloudinary_categories.py
@@ -4,12 +4,12 @@ from django.utils.text import slugify
 def populate_categories(apps, schema_editor):
     Category = apps.get_model('categories', 'Category')
     categories = [
-        {"name": "String Instruments", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/v1234567890/echo_project/category-icons/string-instruments.png", "order": 1},
-        {"name": "Percussion", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/v1234567890/echo_project/category-icons/percussion.png", "order": 4},
-        {"name": "Wind Instruments", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/v1234567890/echo_project/category-icons/wind-instruments.png", "order": 3},
-        {"name": "Key Instruments", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/v1234567890/echo_project/category-icons/key-instruments.png", "order": 2},
-        {"name": "Speakers and Studio Equipment", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/v1234567890/echo_project/category-icons/speakers-and-studio-equipment.png", "order": 5},
-        {"name": "Other", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/v1234567890/echo_project/category-icons/other.png", "order": 6},
+        {"name": "String Instruments", "icon": "string-instruments_kyygjw", "order": 1},
+        {"name": "Key Instruments", "icon": "key-instruments_r8tf6b", "order": 2},
+        {"name": "Wind Instruments", "icon": "wind-instruments_ymljd9", "order": 3},
+        {"name": "Percussion", "icon": "percussion_ysdixz", "order": 4},
+        {"name": "Speakers and Studio Equipment", "icon": "speakers-and-studio-_equipment_dz8mp0", "order": 5},
+        {"name": "Other", "icon": "other_uokmuf", "order": 6},
     ]
 
     for cat in categories:
@@ -32,3 +32,4 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunPython(populate_categories),
     ]
+
