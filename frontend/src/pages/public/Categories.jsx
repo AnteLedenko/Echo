cd131d9484bbcde60c4cac6e70db2f4bf2067986
Ante Ledenko <ledenk96@gmail.com>
2025-04-09 19:56:38 +0100

    v1

--- frontend/src/pages/public/Categories.jsx
+++ frontend/src/pages/public/Categories.jsx
@@ -31,7 +31,7 @@ const Categories = () => {
             >
               {cat.icon && (
                 <img
-                src={`${import.meta.env.VITE_CLOUDINARY_BASE_URL}/image/upload/1/${cat.icon}`}
+                src={`${import.meta.env.VITE_CLOUDINARY_BASE_URL}/image/upload/v1/${cat.icon}`}
                 alt={cat.name}
                 className="w-20 h-20 mx-auto mb-2 object-contain"
               />
