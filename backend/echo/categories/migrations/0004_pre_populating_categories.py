ef13b4338e761750649e726392c8e8af68c111b0
Ante Ledenko <ledenk96@gmail.com>
2025-04-09 12:13:36 +0100

    pre populating categories fix2

--- backend/echo/categories/migrations/0004_pre_populating_categories.py
+++ backend/echo/categories/migrations/0004_pre_populating_categories.py
@@ -4,12 +4,12 @@ from django.utils.text import slugify
 def create_categories(apps, schema_editor):
     Category = apps.get_model('categories', 'Category')
     categories = [
-        {"name": "String Instruments", "icon": "category_icons/string-instruments.png", "order": 1},
-        {"name": "Percussion", "icon": "category_icons/percussion.png", "order": 4},
-        {"name": "Wind Instruments", "icon": "category_icons/wind-instruments.png", "order": 3},
-        {"name": "Key Instruments", "icon": "category_icons/key-instruments.png", "order": 2},
-        {"name": "Speakers and Studio Equipment", "icon": "category_icons/speakers-and-studio-equipment.png", "order": 5},
-        {"name": "Other", "icon": "category_icons/other.png", "order": 6},
+        {"name": "String Instruments", "icon": "/media/category-icons/string-instruments.png", "order": 1},
+        {"name": "Percussion", "icon": "/media/category-icons/percussion.png", "order": 4},
+        {"name": "Wind Instruments", "icon": "/media/category-icons/wind-instruments.png", "order": 3},
+        {"name": "Key Instruments", "icon": "/media/category-icons/key-instruments.png", "order": 2},
+        {"name": "Speakers and Studio Equipment", "icon": "/media/category-icons/speakers-and-studio-equipment.png", "order": 5},
+        {"name": "Other", "icon": "/media/category-icons/other.png", "order": 6},
     ]
 
     for cat in categories:
