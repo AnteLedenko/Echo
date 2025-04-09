919def16ad134382e5392422467c5f96fcbd049e
Ante Ledenko <ledenk96@gmail.com>
2025-04-09 17:00:07 +0100

    i knew it

--- frontend/src/pages/public/Categories.jsx
+++ frontend/src/pages/public/Categories.jsx
@@ -31,7 +31,7 @@ const Categories = () => {
             >
               {cat.icon && (
                 <img
-                  src={`/icons/${cat.icon}`}
+                  src={`${cat.icon}`}
                   alt={cat.name}
                   className="w-20 h-20 mx-auto mb-2 object-contain"
                 />
