fa52d4dd1908b04723adfe171b114827060e9655
Ante Ledenko <ledenk96@gmail.com>
2025-04-10 15:47:48 +0100

    full urls

--- backend/echo/categories/migrations/0006_populate_cloudinary_categories.py
+++ backend/echo/categories/migrations/0006_populate_cloudinary_categories.py
@@ -4,12 +4,12 @@ from django.utils.text import slugify
 def populate_categories(apps, schema_editor):
     Category = apps.get_model('categories', 'Category')
     categories = [
-        {"name": "String Instruments", "icon": "string-instruments_kyygjw", "order": 1},
-        {"name": "Key Instruments", "icon": "key-instruments_r8tf6b", "order": 2},
-        {"name": "Wind Instruments", "icon": "wind-instruments_ymljd9", "order": 3},
-        {"name": "Percussion", "icon": "percussion_ysdixz", "order": 4},
-        {"name": "Speakers and Studio Equipment", "icon": "speakers-and-studio-_equipment_dz8mp0", "order": 5},
-        {"name": "Other", "icon": "other_uokmuf", "order": 6},
+        {"name": "String Instruments", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/string-instruments_kyygjw.png", "order": 1},
+        {"name": "Key Instruments", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/key-instruments_r8tf6b", "order": 2},
+        {"name": "Wind Instruments", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/wind-instruments_ymljd9", "order": 3},
+        {"name": "Percussion", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/percussion_ysdixz", "order": 4},
+        {"name": "Speakers and Studio Equipment", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/speakers-and-studio-_equipment_dz8mp0", "order": 5},
+        {"name": "Other", "icon": "https://res.cloudinary.com/dpabrqxuy/image/upload/other_uokmuf", "order": 6},
     ]
 
     for cat in categories:
