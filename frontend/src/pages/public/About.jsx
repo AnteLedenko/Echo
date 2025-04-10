68a43c919744e97b79f510a44c6158e3c294f175
Ante Ledenko <ledenk96@gmail.com>
2025-04-11 00:56:14 +0100

    path fix

--- frontend/src/pages/public/About.jsx
+++ frontend/src/pages/public/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axiosInstance from "../utils/axiosInstance";
+import axiosInstance from "../../utils/axiosInstance";
 
 const About = () => {
   const [data, setData] = useState({
